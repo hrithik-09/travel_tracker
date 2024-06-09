646f446d0c6b71782a518e8ec75a22ad74c5f54c
hrithik09 <ranjan.hrithik09@gmail.com>
2024-06-10 03:10:22 +0530

    checks for duplicate entry added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from"pg";
+
 const app = express();
 const port = 3000;
 
@@ -17,15 +18,19 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// Home Page
-app.get("/", async (req, res) => {
-  //Write your code here.
+async function checkvisited(){
   const result=await db.query("SELECT country_code FROM visited_countries");
   let countries=[];
   result.rows.forEach(element => {
     countries.push(element.country_code);    
   });
-  console.log(result.rows);
+  // console.log(result.rows);
+  return countries;
+}
+// Home Page
+app.get("/", async (req, res) => {
+  //Write your code here.
+  const countries=await checkvisited();
   res.render("index.ejs",{
     countries:countries,
     total:countries.length
@@ -36,18 +41,39 @@ app.get("/", async (req, res) => {
 // Insert data in the table
 app.post("/add",async(req,res)=>{
   const input=req.body["country"];
-  const result = await db.query(
-    "SELECT country_code FROM countries WHERE country_name = $1",
-    [input]
-  );
-  console.log(result.rows);
-  if (result.rows.length!==0) {
-    const data=result.rows[0];
-    const code=data.country_code;
-    console.log(code);
-    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)",[code,]);
-    res.redirect("/");
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE country_name LIKE '%'$1'%'",
+      [input]
+    );
+    // console.log(result.rows);
+    
+      const data=result.rows[0];
+      const code=data.country_code;
+      // console.log(code);
+    try {
+      await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)",[code,]);
+      res.redirect("/");
+      
+    } catch (error) {
+      // console.log(error);
+      const countries=await checkvisited();
+      res.render("index.ejs",{
+        countries:countries,
+        total:countries.length,
+        error:"Country has already been marked"
+      });
+    }
+  } catch (error) {
+    // console.log(error);
+    const countries=await checkvisited();
+    res.render("index.ejs",{
+      countries:countries,
+      total:countries.length,
+      error:"Country name does not exist"
+    });
   }
+  
 });
 
 app.listen(port, () => {
